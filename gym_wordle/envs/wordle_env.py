8afa88ce03a70df49ecf24a9faadabf0add0a0d9
shaunsim15 <shaunsim_15@hotmail.com>
2023-10-22 22:52:52 -0400

    Add explanatory comments

--- gym_wordle/envs/wordle_env.py
+++ gym_wordle/envs/wordle_env.py
@@ -1,12 +1,17 @@
 import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
-import pkg_resources
-import random
-from typing import Optional
-import colorama
+import pkg_resources # The pkg_resources module distributed with setuptools provides an API for Python libraries to access their resource files
+import random # Generates pseudorandom numbers
+from typing import Optional # This module provides runtime support for type hints  https://docs.python.org/3/library/typing.html
+import colorama # Used for producing colored terminal text across multiple platforms (e.g. Windows) https://pypi.org/project/colorama/
 from colorama import Fore
 from colorama import Style
+# Cross-platform printing of colored text can then be done using Colorama’s constant shorthand for ANSI escape sequences. Can try uncommenting and running file.
+# print(Fore.RED + 'some red text')
+# print(Style.DIM + 'and in dim text')
+# print(Style.RESET_ALL)
+# print('back to normal now')
 
 from gym_wordle.exceptions import InvalidWordException
 
@@ -20,24 +25,26 @@ WORD_LENGTH = 5
 filename = pkg_resources.resource_filename(
     'gym_wordle',
     'data/5_words.txt'
-)
+) # an easier way would be to say filename = '../data/5_words.txt'
 
+# Opposite of strToEncode?
 def encodeToStr(encoding):
     string = ""
     for enc in encoding:
         string += chr(ord('a') + enc)
     return string
 
-def strToEncode(lines):
+# Converts an array of 5-letter words to an array of 5-number-tuples
+def strToEncode(lines): # lines is ["aback", "abase", ...]
     encoding = []
     for line in lines:
         assert len(line.strip()) == 5  # Must contain 5-letter words for now
         encoding.append(tuple(ord(char) - 97 for char in line.strip()))
-    return encoding
+    return encoding # this return variable is an array of tuples: [(0, 1, 0, 2, 10), (0, 1, 0, 18, 4), ...] (note the 2 tuples I showed correspond to 'aback' and 'abase')
 
 
 with open(filename, "r") as f:
-    WORDS = strToEncode(f.readlines())
+    WORDS = strToEncode(f.readlines()) # f.readlines() reads the contents of the file f line by line and returns a list where each element is a line from the file.
 
 
 class WordleEnv(gym.Env):
@@ -80,8 +87,35 @@ class WordleEnv(gym.Env):
     """
 
     def __init__(self):
-        super(WordleEnv, self).__init__()
+        super(WordleEnv, self).__init__() # calls the constructor of the parent class, gym.Env
+
+        # DISCRETE IS ONE OF THE FUNDAMENTAL SPACES
+        # https://gymnasium.farama.org/api/spaces/fundamental/#discrete
+        # It's a space consisting of finitely many elements.
+        # e.g. observation_space = Discrete(2) # {0, 1}
+        #      observation_space.sample() # 0
+
+        # MULTIDISCRETE IS ONE OF THE FUNDAMENTAL SPACES
+        # https://gymnasium.farama.org/api/spaces/fundamental/#gymnasium.spaces.MultiDiscrete
+        # A nintendo controller can be conceptualized as 3 discrete action spaces:
+        # 1) Arrow Keys: 0 [None], 1 [Up], 2[Right], 3[Down], 4[Left] 
+        # 2) Button A: 0 [None], 1 [Pressed] 
+        # 3) Button B: 0 [None], 1 [Pressed] 
+        # You can represent a given action as spaces.MultiDiscrete([ 5, 2, 2 ]). A sample might be array([3, 1, 0])
         self.action_space = spaces.MultiDiscrete([26] * WORD_LENGTH)
+
+        # BOX IS ONE OF THE FUNDAMENTAL SPACES
+        # It's a (possibly unbounded) box in R^n. 
+        # Imagine a 2D square on a graph with bottom LHS corner at (1, 1) & top RHS corner at (2, 2)
+        # The shape of the space is 2D space with "individual" elements, so we say (I think): shape=(2,)
+        # Box(low=-1.0, high=2.0, shape=(2, ), dtype=np.float32)
+        # Sampling from the box is kinda like picking real-valued coordinates in the space.
+
+        # DICT IS ONE OF THE COMPOSITE SPACES
+        # The keys of a dict are whatever you like, the values are one of the fundamental (or composite!) spaces
+        # e.g. observation_space = Dict({"colorA": Discrete(3), "colorB": Discrete(3)})
+        #      observation_space.sample() # OrderedDict([('colorA', 2), ('colorB', 0)])
+        # Elements of this space are (ordered) dictionaries of elements from the constituent spaces.
         self.observation_space = spaces.Dict({
             'board': spaces.Box(low=-1, high=2, shape=(GAME_LENGTH, WORD_LENGTH), dtype=int),
             'alphabet': spaces.Box(low=-1, high=2, shape=(26,), dtype=int)
